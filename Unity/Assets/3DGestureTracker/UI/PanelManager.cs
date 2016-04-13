dc6b988dfba3aa060f91ff8a7988eb85bcf475d4
Edwon <edwonia@gmail.com>
2016-04-13 16:41:50 +0900

    basic gesture UI working on Vive

---
+++ Unity/Assets/3DGestureTracker/UI/PanelManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PanelManager : MonoBehaviour
+{
+    Animator panelAnim;
+
+    void OnEnable ()
+    {
+        panelAnim = GetComponent<Animator>();
+    }
+
+    public void FocusPanel (string panelName)
+    {
+        panelAnim.SetTrigger(panelName);
+        //GameObject panel = transform.FindChild(panelName).gameObject;
+        //GameObject selectableButton = FindFirstEnabledSelectable(panel);
+        //SetSelected(selectableButton);
+    }
+
+    // UTILITY
+
+	static GameObject FindFirstEnabledSelectable (GameObject gameObject)
+	{
+		GameObject go = null;
+		var selectables = gameObject.GetComponentsInChildren<Selectable> (true);
+		foreach (var selectable in selectables) {
+			if (selectable.IsActive () && selectable.IsInteractable ()) {
+				go = selectable.gameObject;
+				break;
+			}
+		}
+		return go;
+	}
+
+	private void SetSelected(GameObject go)
+	{
+		EventSystem.current.SetSelectedGameObject(go);
+
+		var standaloneInputModule = EventSystem.current.currentInputModule as StandaloneInputModule;
+		if (standaloneInputModule != null && standaloneInputModule.inputMode == StandaloneInputModule.InputMode.Buttons)
+			return;
+
+		EventSystem.current.SetSelectedGameObject(null);
+	}
+}
