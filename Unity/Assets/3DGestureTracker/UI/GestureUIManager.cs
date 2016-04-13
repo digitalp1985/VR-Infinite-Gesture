97f86bf2cb6414208d5ae1b807c05b81ab78896a
Edwon <edwonia@gmail.com>
2016-04-13 15:03:58 +0900

    basic UI laser pointer working

--- Unity/Assets/3DGestureTracker/UI/GestureUIManager.cs
+++ Unity/Assets/3DGestureTracker/UI/GestureUIManager.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class GestureUIManager : MonoBehaviour
 {
+    public VROptions.Handedness handedness;
+    VRHandBase uiHand;
+    Camera uiCam;
+    public float offsetZ;
 
 	void Start ()
     {
-	
+        uiHand = PlayerManager.GetPlayerHand(0, handedness);
+        uiCam = PlayerManager.GetPlayerCamera(0);
 	}
 	
 	void Update ()
     {
-	
-	}
+        Vector3 handToCamVector = uiCam.transform.position - uiHand.transform.position;
+        transform.position = uiHand.transform.position + (offsetZ * handToCamVector);
+        transform.rotation = Quaternion.LookRotation(transform.position - uiCam.transform.position);
+    }
 
 }
