ecc360d7a4a0d180fd9bdd68400f7d18b6c174b6
Edwon <edwonia@gmail.com>
2016-05-26 17:02:59 -0700

    example 3 begins

---
+++ Unity/Assets/3DGestureTracker/Examples/Example 3/Example3Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using WinterMute;
+
+public class Example3Player : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnEnable ()
+    {
+        VRGestureManager.GestureDetectedEvent += OnGestureDetected;
+    }
+
+    void OnDisable ()
+    {
+        VRGestureManager.GestureDetectedEvent -= OnGestureDetected;
+    }
+
+    void OnGestureDetected (string gestureName, double confidence)
+    {
+        //string confidenceString = confidence.ToString().Substring(0, 4);
+        //Debug.Log("detected gesture: " + gestureName + " with confidence: " + confidenceString);
+
+        switch (gestureName)
+        {
+            case "Fire":
+
+                break;
+            case "Earth":
+
+                break;
+            case "Ice":
+
+                break;
+            case "Air":
+
+                break;
+        }
+    }
+
+    IEnumerator AnimateShape (GameObject shape)
+    {
+        yield return null;
+    }
+}
