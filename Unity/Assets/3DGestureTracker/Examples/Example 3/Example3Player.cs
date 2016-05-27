25929dee80d2226ed20e8d73b1f1936ea139b18e
Edwon <edwonia@gmail.com>
2016-05-26 17:27:23 -0700

    fireball shooting is working

--- Unity/Assets/3DGestureTracker/Examples/Example 3/Example3Player.cs
+++ Unity/Assets/3DGestureTracker/Examples/Example 3/Example3Player.cs
@@ -4,15 +4,36 @@ using WinterMute;
 
 public class Example3Player : MonoBehaviour
 {
+    public GameObject fire;
 
-	void Start ()
+    VRAvatar myAvatar;
+    IInput input;
+
+    Transform playerHead;
+    Transform playerHandL;
+    Transform playerHandR;
+
+    void Start ()
     {
-	
-	}
+        myAvatar = PlayerManager.GetPlayerAvatar(0);
+
+        playerHead = myAvatar.headTF;
+        playerHandR = myAvatar.vrRigAnchors.rHandAnchor;
+        playerHandL = myAvatar.vrRigAnchors.lHandAnchor;
+
+        if (Config.handedness == Config.Handedness.Right)
+        {
+            input = myAvatar.GetInput(VROptions.Handedness.Right);
+        }
+        else if (Config.handedness == Config.Handedness.Left)
+        {
+            input = myAvatar.GetInput(VROptions.Handedness.Left);
+        }
+    }
 	
 	void Update ()
     {
-	
+	    
 	}
 
     void OnEnable ()
@@ -33,18 +54,40 @@ public class Example3Player : MonoBehaviour
         switch (gestureName)
         {
             case "Fire":
-
+                DoFire();
                 break;
             case "Earth":
-
+                DoEarth();
                 break;
             case "Ice":
-
+                DoIce();
                 break;
             case "Air":
-
+                DoAir();
                 break;
         }
+    }
+
+    void DoFire ()
+    {
+        Quaternion rotation = Quaternion.LookRotation(playerHandR.forward, Vector3.up);
+        Vector3 betweenHandsPos = (playerHandL.position + playerHandR.position) / 2;
+        GameObject.Instantiate(fire, betweenHandsPos, rotation);
+    }
+
+    void DoEarth ()
+    {
+
+    }
+
+    void DoIce()
+    {
+
+    }
+
+    void DoAir()
+    {
+
     }
 
     IEnumerator AnimateShape (GameObject shape)
---
+++ Unity/Assets/3DGestureTracker/Examples/Example 3/Powers/Fire/FirePower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirePower : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+    {
+        Vector3 force = new Vector3(0, 0, speed);
+        rb.AddRelativeForce(force, ForceMode.Impulse);
+	}
+
+    void OnCollisionEnter ()
+    {
+        Debug.Log("hit something");
+        //Destroy(this);
+    }
+}
