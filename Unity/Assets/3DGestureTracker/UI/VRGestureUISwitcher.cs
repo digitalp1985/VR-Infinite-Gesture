d66bf1f9edf6059b308f71274494ddfb2ba34f0e
Edwon <edwonia@gmail.com>
2016-05-29 17:05:36 -0700

    added VRGestureUISwitcher

---
+++ Unity/Assets/3DGestureTracker/UI/VRGestureUISwitcher.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WinterMute
+{
+
+    public class VRGestureUISwitcher : MonoBehaviour
+    {
+        VRAvatar myAvatar;
+        IInput input;
+
+        Transform playerHead;
+        Transform playerHandL;
+        Transform playerHandR;
+
+        public VRGestureUI vrGestureUI;
+
+        void Start()
+        {
+            myAvatar = PlayerManager.GetPlayerAvatar(0);
+
+            playerHead = myAvatar.headTF;
+            playerHandR = myAvatar.vrRigAnchors.rHandAnchor;
+            playerHandL = myAvatar.vrRigAnchors.lHandAnchor;
+
+            if (Config.handedness == Config.Handedness.Right)
+            {
+                input = myAvatar.GetInput(VROptions.Handedness.Left);
+            }
+            else if (Config.handedness == Config.Handedness.Left)
+            {
+                input = myAvatar.GetInput(VROptions.Handedness.Right);
+            }
+        }
+
+        void Update ()
+        {
+            // if vr button 1 toggle the vr gesture UI visibility
+            if (input.GetButtonDown(InputOptions.Button.Button1))
+            {
+                vrGestureUI.gameObject.SetActive(!vrGestureUI.gameObject.activeInHierarchy);
+            }
+        }
+    }
+}
