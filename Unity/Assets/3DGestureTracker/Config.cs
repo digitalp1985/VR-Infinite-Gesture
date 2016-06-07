3220253560061449ea801ab94d7474b8032b98c8
Edwon <edwonia@gmail.com>
2016-06-06 17:57:16 -0700

    renamed SAVE_DATA folder to VRGestureData

--- Unity/Assets/3DGestureTracker/Config.cs
+++ Unity/Assets/3DGestureTracker/Config.cs
@@ -2,7 +2,7 @@
 {
     public static class Config
     {
-        public const string SAVE_FILE_PATH = @"Assets/3DGestureTracker/SAVE_DATA/";
+        public const string SAVE_FILE_PATH = @"Assets/3DGestureTracker/VRGestureData/";
         public const bool USE_RAW_DATA = true;
         public const int FIDELITY = 11;
         public const int CAPTURE_RATE = 30;
