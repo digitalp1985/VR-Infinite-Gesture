87155f000915513e834add9011f28a4bd073e298
Edwon <edwonia@gmail.com>
2016-06-13 19:27:41 -0700

    put extensionsmethods and IInput in WinterMute namespace

--- Unity/Assets/3DGestureTracker/Tywon/VR/Input/IInput.cs
+++ Unity/Assets/3DGestureTracker/Tywon/VR/Input/IInput.cs
@@ -1,51 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class InputOptions
+namespace WinterMute
 {
-    public enum Button
+    public static class InputOptions
     {
-        Button1,
-        Button2,
-        Button3,
-        Button4,
-        Directional1,
-        Trigger1,
-        Trigger2,
-        Platform
-    };
-    public enum Touch
-    {
-        Trigger1,
-        Trigger2
+        public enum Button
+        {
+            Button1,
+            Button2,
+            Button3,
+            Button4,
+            Directional1,
+            Trigger1,
+            Trigger2,
+            Platform
+        };
+        public enum Touch
+        {
+            Trigger1,
+            Trigger2
+        }
+        public enum Axis1D
+        {
+            Trigger1,
+            Trigger2
+        };
+        public enum Axis2D
+        {
+            Directional1
+        };
     }
-    public enum Axis1D
+
+    public interface IInput
     {
-        Trigger1,
-        Trigger2
-    };
-    public enum Axis2D
-    {
-        Directional1
-    };
-}
+        bool GetButton(InputOptions.Button button);
 
-public interface IInput
-{
-    bool GetButton(InputOptions.Button button);
+        bool GetButtonDown(InputOptions.Button button);
 
-    bool GetButtonDown(InputOptions.Button button);
+        bool GetButtonUp(InputOptions.Button button);
 
-    bool GetButtonUp(InputOptions.Button button);
+        bool GetTouch(InputOptions.Touch touch);
 
-    bool GetTouch(InputOptions.Touch touch);
+        bool GetTouchDown(InputOptions.Touch touchDown);
 
-    bool GetTouchDown(InputOptions.Touch touchDown);
+        float GetAxis1D(InputOptions.Axis1D axis1D);
 
-    float GetAxis1D(InputOptions.Axis1D axis1D);
+        Vector2 GetAxis2D(InputOptions.Axis2D axis2D);
 
-    Vector2 GetAxis2D(InputOptions.Axis2D axis2D);
+        void InputUpdate();
 
-    void InputUpdate();
-
+    }
 }
