b8b0216d106a2e71d5d5914721b0bc302d7b2168
Edwon <edwonia@gmail.com>
2016-06-14 19:04:31 -0700

    split the one playmaker action into two, VRGestureDetectedEvent and VRGestureNullEvent

---
+++ Unity/Assets/3DGestureTracker/Integrations/Playmaker/Actions/VRGestureDetectedEvent.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Edwon.VR.Gesture;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+    [ActionCategory("VRGestureTracker")]
+    [Tooltip ("Listens for gestures detected with the Edwon VR Gesture Tracker plugin")]
+    public class VRGestureDetectedEvent : FsmStateAction
+    {
+        public FsmString gestureName;
+        public FsmEvent gestureDetectedEvent;
+
+        public override void Reset()
+        {
+            gestureName = null;
+            gestureDetectedEvent = null;
+        }
+
+        // Code that runs on entering the state.
+        public override void OnEnter()
+	    {
+            VRGestureManager.GestureDetectedEvent += OnGestureDetected;
+        }
+
+	    // Code that runs when exiting the state.
+	    public override void OnExit()
+	    {
+            VRGestureManager.GestureDetectedEvent -= OnGestureDetected;
+        }
+
+        void OnGestureDetected (string _gestureName, double _confidence)
+        {
+            if (_gestureName == gestureName.Value)
+            {
+                Fsm.Event(gestureDetectedEvent);
+            }
+        }
+    }
+
+}
---
+++ Unity/Assets/3DGestureTracker/Integrations/Playmaker/Actions/VRGestureNullEvent.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Edwon.VR.Gesture;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+    [ActionCategory("VRGestureTracker")]
+    [Tooltip ("Fires when a gesture is captured but can't be recognized fromt the Edwon VR Gesture Tracker plugin")]
+    public class VRGestureNullEvent : FsmStateAction
+    {
+        public FsmEvent gestureNullEvent;
+
+        public override void Reset()
+        {
+            gestureNullEvent = null;
+        }
+
+        // Code that runs on entering the state.
+        public override void OnEnter()
+	    {
+            VRGestureManager.GestureNullEvent += OnGestureNull;
+        }
+
+	    // Code that runs when exiting the state.
+	    public override void OnExit()
+	    {
+            VRGestureManager.GestureNullEvent -= OnGestureNull;
+        }
+
+        void OnGestureNull ()
+        {
+            Fsm.Event(gestureNullEvent);
+        }
+    }
+
+}
