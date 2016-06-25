5e531284c47a0f08289b9ffe6d609c6e2812eb3e
Edwon <edwonia@gmail.com>
2016-06-25 09:41:53 -0700

    created custom popup test, solves selected popup problem in a constrained evironment

---
+++ Unity/Assets/Edwon/VR/Gesture Dev/Tests/Custom Popup Test/CustomPopupTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Edwon.VR.Gesture.Test
+{
+	public class CustomPopupTest : MonoBehaviour
+	{
+		[SerializeField]
+		public List<string> list;
+		[SerializeField]
+		public string choice;
+	}
+}
