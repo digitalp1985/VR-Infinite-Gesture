83b85990267c16ea0045ef885820e077350ddf6d
Edwon <edwonia@gmail.com>
2016-12-07 14:36:35 -0800

    getting started tutorial version begins

---
+++ Unity/Assets/Edwon/VR/Gesture/Examples/Getting Started/GettingStartedTutorial.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GettingStartedTutorial : MonoBehaviour
+{
+
+    public Renderer movieRenderer;
+    MovieTexture movieTexture;
+
+    void OnEnable()
+    {
+        //EditorApplication.update += EditorUpdate;
+    }
+
+    static void EditorUpdate()
+    {
+        //Debug.Log("editor update");
+    }
+
+    void OnRenderObject()
+    {
+        //Debug.Log("on render object");
+        if (movieRenderer != null)
+        {
+            if (movieTexture == null)
+            {
+                movieTexture = (MovieTexture)movieRenderer.sharedMaterial.mainTexture;
+            }
+            else
+            {
+                if (!movieTexture.isPlaying)
+                {
+                    movieTexture.loop = true;
+                    movieTexture.Play();
+                }
+            }
+        }
+    }
+
+}
