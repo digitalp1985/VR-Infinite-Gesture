46169edbbbf8ae82251daf4e266b15ad8e03849d
Edwon <edwonia@gmail.com>
2016-12-19 17:02:37 -0800

    tutorial initialize on load test

---
+++ Unity/Assets/Edwon/VR/Gesture/Tutorial/Scripts/TutorialInitializeOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[InitializeOnLoad]
+public class TutorialInitializeOnLoad
+{
+    static TutorialInitializeOnLoad()
+    {
+        Debug.Log("Autorun!");
+    }
+}
+
+
