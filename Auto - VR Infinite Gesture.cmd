e9a97820c1fc84daa78294edb9e9f6f0ee2ec260
Edwon <edwonia@gmail.com>
2017-01-02 18:51:04 -0800

    added some ECHO and made cmd close with auto opener script thingy

--- Auto - VR Infinite Gesture.cmd
+++ Auto - VR Infinite Gesture.cmd
@@ -6,8 +6,11 @@ SET UnityPath="%ProgramFiles%/Unity 5.4.3f1/Editor/Unity.exe"
 SET ProjectPath="%USERPROFILE%/Projects/VR-Infinite-Gesture/Unity"
 SET Method1="CommandLineMethods.OpenScriptEditor"
 
+ECHO VR-Infinite-Gesture
+ECHO Unity 5.4.3f1
+
 REM this command opens unity with the given project and runs the method given
-%UnityPath% -projectPath %ProjectPath% -executeMethod %Method1%
+START "Unity" %UnityPath% -projectPath %ProjectPath% -executeMethod %Method1%
 
 EXIT
 
